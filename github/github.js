97629876a1b2f4435734905bb2822b050c9120e1
prady <pradhyumpatel@gmail.com>
2020-12-10 16:44:05 -0600

    Add html, css, and js to sucessfully fetch user profiles

--- github/github.js
+++ github/github.js
@@ -2,5 +2,17 @@ var form = document.querySelector('#github-search');
 
 form.addEventListener('submit', function(event) {
   event.preventDefault();
-  console.log("Form Submitted!!");
+  var username = document.querySelector('#username').value;
+  console.log("Form Submitted!, We are Searching for", username);
+  fetch('https://api.github.com/users/' + username)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data){
+      var info_list = document.querySelector('#results');
+      info_list.querySelector('.username b').innerText = data.login;
+      info_list.querySelector('.name b').innerText = data.name;
+      info_list.querySelector('.photo img').src = data.avatar_url;
+    });
+    document.querySelector('#username').value = '';
 });
